e6128063a1466cc3d434dd32d42797235202c734
nate-owens <nowens@ccgideas.com>
2021-10-01 15:15:12 -0500

    finished part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,32 +1,39 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout from '../components/layout';
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-          {
-            data.allFile.nodes.map(node => {
-                if (node.ext === '.mdx') {
-                    return (<li key={node.name}>{node.name}</li>)
-                } else {
-                    return ''
-                }
+        {
+            data.allMdx.nodes.map(node => {
+                return (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <MDXRenderer>
+                            {node.body}
+                        </MDXRenderer>
+                    </article>
+                )
             })
-          }
-      </ul>
+        }
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile {
-      nodes {
-        name
-        ext
-      }
+    allMdx(sort: {order: DESC, fields: frontmatter___date}) {
+        nodes {
+            frontmatter {
+                title
+                date(formatString: "MMMM D, YYYY")
+            }
+            id
+            body
+        }
     }
   }
 `
