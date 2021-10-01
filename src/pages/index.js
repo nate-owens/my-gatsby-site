da1c5e40f4980578b327844f99a025f9b1e0eab9
nate-owens <nowens@ccgideas.com>
2021-10-01 16:14:37 -0500

    linking to the gatsby tutorial, because why not?

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,12 @@ import { StaticImage } from 'gatsby-plugin-image';
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>
+        I'm making this by following the 
+        <a href="https://www.gatsbyjs.com/docs/tutorial/" title="_blank">
+          Gatsby Tutorial
+        </a>.
+      </p>
       <StaticImage 
         alt="Clifford, a reddish-brown pitbull lounging on a couch."
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large" 
